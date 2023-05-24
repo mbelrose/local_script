bb767e086188a2a8baa861b89b98b5ed1edbc06f
mbelrose <mike@belrose.net>
2023-05-24 15:52:06 -0500

    no need for delay

--- linux_boot_jobs.sh
+++ linux_boot_jobs.sh
@@ -1,11 +1,11 @@
 #!
 
 # prompt the user to run discord
-sleep 120
 if zenity --question --text="Do you want to run Discord?"; then
     nohup /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=discord com.discordapp.Discord >/dev/null 2>/dev/null &
 fi
 
+# prompt the user to run google voice
 if zenity --question --text="Do you want to run Google Voice?"; then
     nohup chromium https://voice.google.com/ >/dev/null 2>/dev/null &
 fi
