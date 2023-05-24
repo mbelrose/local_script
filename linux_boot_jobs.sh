1c366d54f93979540f9a4295f3407879a4fc5454
mbelrose <mike@belrose.net>
2023-05-24 15:14:59 -0500

    add google voice

--- linux_boot_jobs.sh
+++ linux_boot_jobs.sh
@@ -2,6 +2,10 @@
 
 # prompt the user to run discord
 sleep 120
-if zenity --question --text="Do you want to play music?"; then
-    /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=discord com.discordapp.Discord
+if zenity --question --text="Do you want to run Discord?"; then
+    nohup /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=discord com.discordapp.Discord >/dev/null 2>/dev/null &
+fi
+
+if zenity --question --text="Do you want to run Google Voice?"; then
+    nohup chromium https://voice.google.com/ >/dev/null 2>/dev/null &
 fi
