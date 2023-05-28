c95bf4f0186c54f5535045d98fa7d526a3bc909e
mbelrose <mike@belrose.net>
2023-05-28 04:35:09 -0500

    initial

---
+++ invidious.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/zsh
+
+timeout=60
+working_directory=$HOME/.local/bin/invidious/
+invidious_server_url="http://127.0.0.1:3300/"
+
+# initialize firefox so further calls don't fork processes
+nohup firefox -private >/dev/null 2>/dev/null &
+
+cd $working_directory
+
+# start docker
+if ! systemctl is-active --quiet docker;
+then 
+        sudo systemctl start docker
+fi
+
+# wait for docker to start
+docker_loop_counter=0
+while [[ $docker_loop_counter -le $timeout ]]
+do
+        sleep 1
+        if systemctl is-active --quiet docker;
+        then
+
+# --------
+# --------
+# --------
+
+# start invidious server
+if !  curl -s $invidious_server_url --output /dev/null;
+then
+        docker compose -f docker-compose-local.yml up -d
+fi
+
+# wait for invidious server to start
+invidious_container_loop_counter=0
+while [[ $invidious_container_loop_counter -le $timeout ]]
+do
+        sleep 1
+        if  curl -s $invidious_server_url --output /dev/null;
+        then
+                # open invidious server in firefox
+                firefox -private -url $invidious_server_url >/dev/null 2>/dev/null
+                break
+        fi
+        ((invidious_container_loop_counter++))
+done
+
+# -------
+# -------
+# -------
+                break
+        fi
+        ((docker_loop_counter++))
+done
