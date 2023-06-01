f8d33e6f8c10b0155d5716a3f5b2ceb1e2118aa5
mbelrose <mike@belrose.net>
2023-06-01 03:43:02 -0500

    fix regex to handle more URL variants

--- twitch_streamlink_scripts/twitch.sh
+++ twitch_streamlink_scripts/twitch.sh
@@ -1,4 +1,5 @@
 echo 'Enter streamer id:'
 read streamer_id
-streamer_id=$(echo $streamer_id | sed -E s/https...\(www.\)?twitch.tv.//)
+streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
+streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
 streamlink --twitch-disable-ads https://twitch.tv/$streamer_id
