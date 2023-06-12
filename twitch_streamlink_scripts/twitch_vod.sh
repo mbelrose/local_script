dcf1b7e94764ae0614146bbbe78447f58f5fd6d7
mbelrose <mike@belrose.net>
2023-06-12 08:22:00 -0500

    allow timestamp characters

--- twitch_streamlink_scripts/twitch_vod.sh
+++ twitch_streamlink_scripts/twitch_vod.sh
@@ -1,6 +1,6 @@
 echo 'Enter vod id:'
 read streamer_id
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
-streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_]//g')
+streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
 streamlink --twitch-disable-ads --player-no-close https://twitch.tv/videos/$streamer_id
 
