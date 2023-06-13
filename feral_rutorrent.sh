7bca5b728c34722b839043c51e3e88412c3c659f
mbelrose <mike@belrose.net>
2023-06-13 01:38:36 -0500

    left stub in

--- feral_rutorrent.sh
+++ feral_rutorrent.sh
@@ -4,5 +4,4 @@ password=$(cat '/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI/Documents/loca
 rutorrent_url="https://mbelrose:${password}@phanes.feralhosting.com/mbelrose/rutorrent/"
 
 nohup firefox -private $rutorrent_url >/dev/null 2>/dev/null &
-read foo
  
