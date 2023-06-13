08cbb6dbbe90ce86eaddf60ecf7e8c06b6a95a30
mbelrose <mike@belrose.net>
2023-06-13 04:55:21 -0500

    passwords in url is deprecated =

--- feral_rutorrent.ps1
+++ feral_rutorrent.ps1
@@ -1,2 +1,1 @@
- $password = Get-Content 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\keys\feral_rutorrent_password.txt'
- & 'C:\Program Files\Mozilla Firefox\firefox.exe'  -private -URL ('https://mbelrose:' + $password[0] + '@phanes.feralhosting.com/mbelrose/rutorrent/')
+  & 'C:\Program Files\Mozilla Firefox\firefox.exe'  -private -URL ('https://mbelrose@phanes.feralhosting.com/mbelrose/rutorrent/')
