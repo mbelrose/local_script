749852aa9acf827347f2a8fcb002f5c464b2e53d
mbelrose <mike@belrose.net>
2023-06-13 01:12:54 -0500

    wrong filenames

--- feral_rutorrent.ps1
+++ feral_rutorrent.ps1
@@ -1,2 +1,2 @@
- $password = Get-Content 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\keys\feral_rclone_password'
+ $password = Get-Content 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\keys\feral_rutorrent_password.txt'
  & 'C:\Program Files\Mozilla Firefox\firefox.exe'  -private -URL ('https://mbelrose:' + $password[0] + '@phanes.feralhosting.com/mbelrose/rutorrent/')
