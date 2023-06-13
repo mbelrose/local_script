ddd6662f5138b0109b7943e5cc073d4f2d389341
mbelrose <mike@belrose.net>
2023-06-13 03:35:27 -0500

    test

--- feral_rutorrent.sh
+++ feral_rutorrent.sh
@@ -3,5 +3,6 @@
 password=$(cat '/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI/Documents/local_script/keys/feral_rutorrent_password.txt')
 rutorrent_url="https://mbelrose:${password}@phanes.feralhosting.com/mbelrose/rutorrent/"
 
+
 nohup firefox -private $rutorrent_url >/dev/null 2>/dev/null &
  
