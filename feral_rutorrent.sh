4a86ac3a739beeb068cc28de80c1e3d8c602b16e
mbelrose <mike@belrose.net>
2023-06-13 01:37:52 -0500

    password was hardcoded

--- feral_rutorrent.sh
+++ feral_rutorrent.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/zsh
+
 password=$(cat '/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI/Documents/local_script/keys/feral_rutorrent_password.txt')
-/usr/bin/firefox -private https://mbelrose:Breadoil1@phanes.feralhosting.com/mbelrose/rutorrent/
+rutorrent_url="https://mbelrose:${password}@phanes.feralhosting.com/mbelrose/rutorrent/"
+echo 'Enter vod id:'
+read streamer_id
+
+nohup firefox -private $rutorrent_url >/dev/null 2>/dev/null &
+read foo
+ 
