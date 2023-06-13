bef54ae83e4a08757978a126af8ba8a5eaf98848
mbelrose <mike@belrose.net>
2023-06-13 07:02:10 -0500

    no need for command line options

--- twitch_streamlink_scripts/twitch.sh
+++ twitch_streamlink_scripts/twitch.sh
@@ -2,4 +2,4 @@ echo 'Enter streamer id:'
 read streamer_id
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
-streamlink --twitch-disable-ads https://twitch.tv/$streamer_id
+streamlink https://twitch.tv/$streamer_id
--- twitch_streamlink_scripts/twitch_vod.sh
+++ twitch_streamlink_scripts/twitch_vod.sh
@@ -2,5 +2,5 @@ echo 'Enter vod id:'
 read streamer_id
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
-streamlink --twitch-disable-ads --player-no-close https://twitch.tv/videos/$streamer_id
+streamlink https://twitch.tv/videos/$streamer_id
 
