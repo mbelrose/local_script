48e14cd05af00d9591e19fe4291df5b366bcbe2b
mbelrose <mike@belrose.net>
2023-06-13 02:06:06 -0500

    break reminder windows

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -1,4 +1,10 @@
+# break reminder
+&  'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1'
+
+# Google Voice
 Start-Sleep -Seconds 20
 & 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://voice.google.com"
+
+# Discord
 Start-Sleep -Seconds 20
 & 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://discord.com/channels/@me"
