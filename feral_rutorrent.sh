b534ff1305295442c295df74a46fc776c21955ec
mbelrose <mike@belrose.net>
2023-06-13 01:38:25 -0500

    left stub in

--- feral_rutorrent.sh
+++ feral_rutorrent.sh
@@ -2,8 +2,6 @@
 
 password=$(cat '/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI/Documents/local_script/keys/feral_rutorrent_password.txt')
 rutorrent_url="https://mbelrose:${password}@phanes.feralhosting.com/mbelrose/rutorrent/"
-echo 'Enter vod id:'
-read streamer_id
 
 nohup firefox -private $rutorrent_url >/dev/null 2>/dev/null &
 read foo
