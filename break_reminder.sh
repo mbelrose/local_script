e8f166546a98aa4d7f6e6f34735c27455852a87a
mbelrose <mike@belrose.net>
2023-06-13 02:49:36 -0500

    switch to toast from popup

--- break_reminder.sh
+++ break_reminder.sh
@@ -3,5 +3,5 @@ break_interval=2700 # 45 minutes
 
 while true; do
   sleep $break_interval
-  zenity --info --title="Break Reminder" --timeout=3 --text="Rest Break"
+  notify-send -t 3000 -u low "Rest Break"
 done
