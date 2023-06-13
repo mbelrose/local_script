41809a8e2197ee209a566fd7d7a74abba6cd0094
mbelrose <mike@belrose.net>
2023-06-13 05:06:42 -0500

    bad security process

--- feral_rclone.sh
+++ feral_rclone.sh
@@ -1,3 +1,3 @@
-#!
-password=$(cat '/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI/Documents/local_script/keys/feral_rclone_password.txt')
+#!/usr/bin/zsh
+password="password" # fine for a local script
 rclone rcd --rc-web-gui --rc-user mbelrose --rc-pass $password
