d2192e8661513c2363c81f9910726949fcc50972
mbelrose <mike@belrose.net>
2023-06-14 03:27:40 -0500

    initial bluetooth gui

---
+++ bluetooth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+device_mac="14:43:19:E5:B5:9F"
+device_name="ET1"
+
+# Create a popup with buttons for "on", "off", and "connect"
+choice=$(zenity --question --title="Bluetooth" --text="Turn Bluetooth on or off?" --ok-label="Connect ${device_name}" --cancel-label="Off")
+
+# Launch bluetoothctl with the selected argument
+if [ "$choice" = true ]; then
+    bluetoothctl power on
+    bluetoothctl connect $device_mac
+else
+    bluetoothctl power off
+fi
