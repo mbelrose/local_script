597000f2443064a0ace2992a9500aa996c59989f
mbelrose <mike@belrose.net>
2023-06-14 11:24:27 -0500

    initial

---
+++ twitch_streamlink_scripts/twitch_streamink_link.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Twitch Video Links with Custom Protocol Handler
+// @namespace    twitch-video-links-custom-protocol-handler
+// @version      1
+// @description  Adds a streamlink custom protocol link after videos on Twitch 
+// @match        https://www.twitch.tv/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Get all video links on the page
+    const videoLinks = document.querySelectorAll('a[data-a-target="preview-card-image-link"]');
+
+    // Loop through the video links and add a star next to each one
+    videoLinks.forEach(link => {
+        const star = document.createElement('span');
+        star.innerHTML = ' &#9733;';
+        link.parentNode.insertBefore(star, link.nextSibling);
+
+        // Create a custom protocol URL with the streamer name as the last part of the URL
+        const streamerName = link.href.split('/')[3];
+        const customProtocolUrl = `myprotocol://${streamerName}`;
+
+        // Create a hyperlink for the custom protocol URL
+        const customProtocolLink = document.createElement('a');
+        customProtocolLink.href = customProtocolUrl;
+        customProtocolLink.innerHTML = '&#9733;';
+        link.parentNode.insertBefore(customProtocolLink, link.nextSibling.nextSibling);
+    });
+})();
