8cc36267a5e6eb03dd469f8a54d1e0694eece508
mbelrose <mike@belrose.net>
2023-06-14 03:35:52 -0500

    unneeded interpolation

--- bluetooth.sh
+++ bluetooth.sh
@@ -4,7 +4,7 @@ device_mac="14:43:19:E5:B5:9F"
 device_name="ET1"
 
 # Create a popup with buttons for "on", "off", and "connect"
-$(zenity --question --title="Bluetooth" --text="Turn Bluetooth on or off?" --ok-label="Connect ${device_name}" --cancel-label="Off")
+zenity --question --title="Bluetooth" --text="Turn Bluetooth on or off?" --ok-label="Connect ${device_name}" --cancel-label="Off"
 
 # Launch bluetoothctl with the selected argument
 if [ $? -eq 0 ]; then
