272a9d2dfd89adf077d39f3186cd7e60f150dc67
mbelrose <mike@belrose.net>
2023-06-14 12:00:13 -0500

    add a delay because too hard to sync

--- twitch_streamlink_scripts/twitch_streamink_link.js
+++ twitch_streamlink_scripts/twitch_streamink_link.js
@@ -10,19 +10,25 @@
 (function() {
     'use strict';
 
-    // Get all video links on the page
-    const videoLinks = document.querySelectorAll('a[data-a-target="preview-card-image-link"]');
+    // Wait 4 seconds for the page to load because the video links are loaded asynchronously
+    setTimeout( () => {
+        
+        // Get all video links on the page
+        const videoLinks = document.querySelectorAll('a[data-a-target="preview-card-image-link"]');
 
-    // Loop through the video links and add a star next to each one
-    videoLinks.forEach(link => {
-        // Create a custom protocol URL with the streamer name as the last part of the URL
-        const streamerName = link.href.split('/')[3];
-        const customProtocolUrl = `myprotocol://${streamerName}`;
+        // Loop through the video links and add a star next to each one
+        videoLinks.forEach(link => {
+            // Create a custom protocol URL with the streamer name as the last part of the URL
+            const streamerName = link.href.split('/')[3];
+            const customProtocolUrl = `myprotocol://${streamerName}`;
 
-        // Create a hyperlink for the custom protocol URL
-        const customProtocolLink = document.createElement('a');
-        customProtocolLink.href = customProtocolUrl;
-        customProtocolLink.innerHTML = '&#9733;';
-        link.parentNode.insertBefore(customProtocolLink, link.nextSibling);
-    });
+            // Create a hyperlink for the custom protocol URL
+            const customProtocolLink = document.createElement('a');
+            customProtocolLink.href = customProtocolUrl;
+            customProtocolLink.innerHTML = '&#9733;';
+            link.parentNode.insertBefore(customProtocolLink, link.nextSibling);
+        });
+
+    }, 4000);
+
 })();
