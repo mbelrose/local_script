4747b6f8f7d68563368472aff3b9aaafb14cb78c
mbelrose <mike@belrose.net>
2023-06-14 03:35:07 -0500

    zenity returns are weird

--- bluetooth.sh
+++ bluetooth.sh
@@ -4,10 +4,10 @@ device_mac="14:43:19:E5:B5:9F"
 device_name="ET1"
 
 # Create a popup with buttons for "on", "off", and "connect"
-choice=$(zenity --question --title="Bluetooth" --text="Turn Bluetooth on or off?" --ok-label="Connect ${device_name}" --cancel-label="Off")
+$(zenity --question --title="Bluetooth" --text="Turn Bluetooth on or off?" --ok-label="Connect ${device_name}" --cancel-label="Off")
 
 # Launch bluetoothctl with the selected argument
-if [ "$choice" = true ]; then
+if [ $? -eq 0 ]; then
     bluetoothctl power on
     bluetoothctl connect $device_mac
 else
