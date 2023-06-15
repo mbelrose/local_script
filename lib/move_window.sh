10caea486d53aef435955e55aaee6e11b4a71cb1
mbelrose <mike@belrose.net>
2023-06-15 15:48:24 -0500

    add more loop logic

--- lib/move_window.sh
+++ lib/move_window.sh
@@ -3,25 +3,35 @@ function move_window() {
 
     local window_title=$1
     local desktop_number=$2
+    SEARCH_LOOP_LIMIT=10
+    MOVE_LOOP_LIMIT=10
 
     # match the window title, partial match, caseless
-    window_list=$(wmctrl -l)
-    window_list=$(
-        awk -v title="$window_title" \
-        'BEGIN {IGNORECASE=1} $0 ~ title { print $1 }' \
-        <<< "$window_list")
-    window_list=$( \
-        grep '^[[:alnum:]]\+$' <<< "$window_list" \
-    )
+    search_loop_counter=0
+    window_list=""
+    while [ ${#window_list} -eq 0 ] \
+        && [ "$search_loop_counter" -le "$SEARCH_LOOP_LIMIT" ]
+    do
+        sleep 1
+        window_list=$(wmctrl -l)
+        window_list=$(
+            awk -v title="$window_title" \
+            'BEGIN {IGNORECASE=1} $0 ~ title { print $1 }' \
+            <<< "$window_list")
+        window_list=$( \
+            grep '^[[:alnum:]]\+$' <<< "$window_list" \
+        )
+        ((search_loop_counter++))
+    done
 
     # move the window
     while IFS= read -r window_id; do
-        if [ ${#window_id} -gt 0 ]
+        if [ ${#window_id} -gt 0 ] # this probably contradicts the while loop
         then
             current_desktop=-1
             loop_counter=0
             while [ "$desktop_number" -ne "$current_desktop" ] \
-                && [ "$loop_counter" -le "$LOOP_LIMIT" ]
+                && [ "$loop_counter" -le "$MOVE_LOOP_LIMIT" ]
             do
                 sleep 1
                 wmctrl -ir "$window_id" -t "$desktop_number"
