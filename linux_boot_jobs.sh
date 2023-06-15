d7d15ca6e4cbbfbc48d946a048df627299b2364d
mbelrose <mike@belrose.net>
2023-06-15 05:49:01 -0500

    ids are alphanumeric,

--- linux_boot_jobs.sh
+++ linux_boot_jobs.sh
@@ -8,18 +8,15 @@ function move_window() {
     local window_title=$1
     local desktop_number=$2
     window_list=$(wmctrl -l)
-    if [[ $(echo "$window_list" | wc -l) -lt 1 ]]; then
-        return 0
-    fi
     window_list=$(
         awk -v title="$window_title" \
         'BEGIN {IGNORECASE=1} $0 ~ title { print $1 }' \
         <<< "$window_list")
+    window_list=$( \
+        grep '^[[:alnum:]]\+$' <<< "$window_list" \
+    )
     while read -r window_id; do
-        if echo "$window_id" | grep -q '^[0-9x]\+$'
-        then
-            wmctrl -ir "$window_id" -t "$desktop_number"
-        fi
+        wmctrl -ir "$window_id" -t "$desktop_number"
     done <<< "$window_list"
 }
 
