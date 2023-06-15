b513a2dc77f47ebe1377fafe52eddb217657a9e9
mbelrose <mike@belrose.net>
2023-06-15 03:36:18 -0500

    cleanup and put things in second desktop

--- linux_boot_jobs.sh
+++ linux_boot_jobs.sh
@@ -11,25 +11,42 @@ zenity --question --text="Do you want to run Twitch Alerts?"
 twitch_alerts=$?
 
 # break reminder
-nohup gnome-terminal --title "Break Reminder" -- $windows_home/Documents/local_script/break_reminder.sh  >/dev/null 2>/dev/null &
+nohup wmctrl -s 1 \
+&& gnome-terminal --title "Break Reminder" \
+-- $windows_home/Documents/local_script/break_reminder.sh \
+>/dev/null 2>/dev/null \
+&& nohup wmctrl -s 0 &
 
 # run twitch alerts
 if [ $twitch_alerts -eq 0 ]; then
-    nohup gnome-terminal --working-directory $working_directory -- node server/TwitchLiveAlert.js  >/dev/null 2>/dev/null &
+    nohup wmctrl -s 1 \
+&& gnome-terminal --title "Twitch Live Alert" \
+--working-directory $working_directory \
+-- node server/TwitchLiveAlert.js  \
+>/dev/null 2>/dev/null \
+&& nohup wmctrl -s 0 &
 fi
 
 # run discord
 if [ $discord -eq 0 ]; then
-    nohup sleep 30 && chromium https://discord.com/channels/@me >/dev/null 2>/dev/null &
+    nohup sleep 1 && wmctrl -s 1 \
+&& chromium https://discord.com/channels/@me \
+>/dev/null 2>/dev/null \
+&& nohup wmctrl -s 0 &
 fi
 
 # run google voice
 if [ $google_voice -eq 0 ]; then
-    nohup sleep 60 && chromium https://voice.google.com/ >/dev/null 2>/dev/null &
+    nohup sleep 1 && wmctrl -s 1 \
+&& chromium https://voice.google.com/ \
+>/dev/null 2>/dev/null \
+&& nohup wmctrl -s 0 &
 fi
 
 # clear thumbnails
 trash-put /home/mint/.cache/thumbnails/
 
 # change wallpaper
-nohup sleep 120 && /usr/bin/variety --profile /home/mint/.config/variety/ >/dev/null 2>/dev/null &
+nohup sleep 120 \
+&& /usr/bin/variety --profile /home/mint/.config/variety/ \
+>/dev/null 2>/dev/null &
