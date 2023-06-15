850637006e7a371c4988687bf6a7403b44f59c4d
mbelrose <mike@belrose.net>
2023-06-15 04:44:36 -0500

    cleanup whitespace

--- linux_boot_jobs.sh
+++ linux_boot_jobs.sh
@@ -27,30 +27,30 @@ twitch_alerts=$?
 
 # break reminder
 nohup gnome-terminal --title "Break Reminder" \
--- $windows_home/Documents/local_script/break_reminder.sh \
->/dev/null 2>/dev/null &
+    -- $windows_home/Documents/local_script/break_reminder.sh \
+    >/dev/null 2>/dev/null &
 move_window_to_desktop "Break Reminder" 1
 
 # run twitch alerts
 if [ $twitch_alerts -eq 0 ]; then
     nohup gnome-terminal --title "Twitch Live Alert" \
---working-directory $working_directory \
--- node server/TwitchLiveAlert.js  \
->/dev/null 2>/dev/null &
+        --working-directory $working_directory \
+        -- node server/TwitchLiveAlert.js  \
+        >/dev/null 2>/dev/null &
     move_window_to_desktop "Twitch Live Alert" 1
 fi
 
 # run discord
 if [ $discord -eq 0 ]; then
     nohup chromium https://discord.com/channels/@me \
->/dev/null 2>/dev/null &
+        >/dev/null 2>/dev/null &
     move_window_to_desktop "Discord" 1
 fi
 
 # run google voice
 if [ $google_voice -eq 0 ]; then
     nohup chromium https://voice.google.com/ \
->/dev/null 2>/dev/null &
+        >/dev/null 2>/dev/null &
     move_window_to_desktop "Voice" 1
 fi
 
