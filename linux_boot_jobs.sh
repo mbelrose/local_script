15953de739e94ba55f1e4af0c0eaea2069e4eb4d
mbelrose <mike@belrose.net>
2023-06-15 16:03:43 -0500

    add firefox

--- linux_boot_jobs.sh
+++ linux_boot_jobs.sh
@@ -45,6 +45,9 @@ fi
 trash-put /home/mint/.cache/thumbnails/ \
     >/dev/null 2>/dev/null &
 
+# initialize firefox
+nohup firefox -private >/dev/null 2>/dev/null &
+
 # switch to main desktop in case got pulled
 wmctrl -s 0
 
