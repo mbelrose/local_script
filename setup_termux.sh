6b25ea4080830a660cb7d48ceccb59c9169f351c
mbelrose <mike@belrose.net>
2023-06-16 15:05:59 -0500

    termux, streamlink, and prereqs

--- setup_termux.sh
+++ setup_termux.sh
@@ -4,7 +4,13 @@ pkg install termux-api
 pkg install openssh
 pkg install git
 pkg install p7zip
+pkg install zip
 pkg install rclone
 pkg install nodejs
 pkg install python
 pkg install zsh
+
+pkg install libxslt
+pkg install streamlink
+pkg install ffmpeg
+pkg install yt-dlp
