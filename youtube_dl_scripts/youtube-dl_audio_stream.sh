b1758bb06760c7c6d16f587375427698331741ab
mbelrose <mike@belrose.net>
2023-06-16 09:16:55 -0500

    move user agend and referrer hack to config

--- youtube_dl_scripts/youtube-dl_audio_stream.sh
+++ youtube_dl_scripts/youtube-dl_audio_stream.sh
@@ -1,4 +1,3 @@
 echo 'Enter stream id:'
 read stream_url
-vlc `yt-dlp -x --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36' --referer 'https://www.youtube.com/' -g $stream_url`
-
+vlc `yt-dlp -x -g $stream_url`
