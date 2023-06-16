0f05a05eb62a7d7bf565e5fe3f54138dcc91e3f4
mbelrose <mike@belrose.net>
2023-06-16 14:47:34 -0500

    first uname search doesn't work

--- youtube_dl_scripts/lib/get_os_platform.sh
+++ youtube_dl_scripts/lib/get_os_platform.sh
@@ -1,14 +1,5 @@
-function is_desktop {
-    if [[ "$(uname -a)" =~ Linux$ ]]
-    then
-        return 0
-    else
-        return 1
-    fi
-}
-
 function is_android {
-    if [[ "$(uname -a)" =~ Android$ ]]
+    if [[ "$(uname -a)" =~ Android ]]
     then
         return 0
     else
