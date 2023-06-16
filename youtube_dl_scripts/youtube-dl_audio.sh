b23e067f6e4307c83f2e5a68eaf9231c9227e5a2
mbelrose <mike@belrose.net>
2023-06-16 14:40:27 -0500

    fix conditional statements

--- youtube_dl_scripts/youtube-dl_audio.sh
+++ youtube_dl_scripts/youtube-dl_audio.sh
@@ -3,16 +3,17 @@
 LIBRARY_DIR="$( dirname -- $0)/lib"
 source "$LIBRARY_DIR/get_os_platform.sh"
 
-if [ is_desktop() ]; then
+if is_desktop; then
     HOME_DIR='/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI'
-    cd $HOME_DIR/Videos/videos_to_move
-elif [ is_android() ]; then
+    cd "$HOME_DIR/Videos/videos_to_move"
+elif is_android; then
     HOME_DIR='/data/data/com.termux/files/home'
-    cd $HOME_DIR/storage/shared/Download
+    cd "$HOME_DIR/storage/shared/Download"
 else
     echo 'Unknown OS'
     return 1
 fi
+
 
 echo 'Enter video id:'
 read video_url
