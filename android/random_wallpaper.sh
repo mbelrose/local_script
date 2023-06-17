6fa372803a3b274abbcbedfde86b79986ab19fff
mbelrose <mike@belrose.net>
2023-06-17 14:42:31 -0500

    only download indices if command line

--- android/random_wallpaper.sh
+++ android/random_wallpaper.sh
@@ -11,17 +11,24 @@ if [ ! -d "$OUTPUT_DIR" ]; then
 fi
 
 # Get a random photo from a Flickr group
-
-group_id="$1"
-curl_output=$(curl -s "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=<your_api_key>&group_id=$group_id&format=json&nojsoncallback=1")
-echo "$curl_output" >> "$OUTPUT_DIR/flickr_output.txt"
+# if a command line parameter was provided
+if [ $# -gt 0 ]; then
+    group_id="$1"
+    curl_output=$(curl -s /
+        "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=<your_api_key>&group_id=$group_id&format=json&nojsoncallback=1" /
+        )
+    # Strip non-filename safe characters
+    safe_group_id=$(echo "$group_id" | tr -c '[:alnum:]._-' '_')
+    echo "$curl_output" >> "$OUTPUT_DIR/$safe_group_id.txt"
+fi
 
 # Pick a random image from a random file in the directory
 random_file=$(find "$OUTPUT_DIR" -type f | shuf -n 1)
 random_line=$(shuf -n 1 "$random_file")
 random_image=$(echo "$random_line" | cut -d ' ' -f 1)
 
-photo_url=$(echo "$curl_output" | jq -r '.photos.photo[].url_o' | shuf -n 1)
+photo_url=$(echo "$curl_output" / 
+    | jq -r '.photos.photo[].url_o' | shuf -n 1)
 
 # Download the photo to the directory
 temp_file="$OUTPUT_DIR/$(date +%Y-%m-%d_%H-%M-%S).jpg"
