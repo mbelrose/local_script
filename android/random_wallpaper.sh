b7de287d71a3ceb38b8060f168d6e44e8763c5dc
mbelrose <mike@belrose.net>
2023-06-17 14:29:36 -0500

    add multiple index files

--- android/random_wallpaper.sh
+++ android/random_wallpaper.sh
@@ -2,29 +2,34 @@
 
 # Set the directory name
 DIR_NAME="my_wallpapers"
+OUTPUT_DIR="$DIR_NAME/output"
 
-# Create the directory if it doesn't exist
-if [ ! -d "$DIR_NAME" ]; then
-  mkdir "$DIR_NAME"
+# Create the directories if they don't exist
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
 fi
 
 # Get a random photo from a Flickr group
 group_id="$1"
 curl_output=$(curl -s "https://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=<your_api_key>&group_id=$group_id&format=json&nojsoncallback=1")
-echo "$curl_output" > flickr_output.txt
+echo "$curl_output" >> "$OUTPUT_DIR/flickr_output.txt"
 photo_url=$(echo "$curl_output" | jq -r '.photos.photo[].url_o' | shuf -n 1)
 
 # Download the photo to the directory
-temp_file="$DIR_NAME/$(date +%Y-%m-%d_%H-%M-%S).jpg"
+temp_file="$OUTPUT_DIR/$(date +%Y-%m-%d_%H-%M-%S).jpg"
 curl -s "$photo_url" -o "$temp_file"
 
 # List the downloaded files and images in the directory
 echo "Downloaded files:"
-ls "$DIR_NAME"
+ls "$OUTPUT_DIR"
 echo "Images in directory:"
-file "$DIR_NAME"/*
+file "$OUTPUT_DIR"/*
 echo
 
+# Pick a random image from a random file in the directory
+random_file=$(find "$OUTPUT_DIR" -type f | shuf -n 1)
+random_line=$(shuf -n 1 "$random_file")
+random_image=$(echo "$random_line" | cut -d ' ' -f 1)
+
 # Set the wallpaper in Android
-adb push "$temp_file" /sdcard/wallpaper.jpg
-adb shell am start -a android.intent.action.ATTACH_DATA -c android.intent.category.DEFAULT -d file:///sdcard/wallpaper.jpg -t image/jpeg
+adb push "$random_image" /sdcard/wallpaper.jpg
