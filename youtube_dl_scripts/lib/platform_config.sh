949d6909b376afed8992a2990e28aa0d607bdccb
mbelrose <mike@belrose.net>
2023-06-21 11:01:54 -0500

    android filesystem standards are not consistent

--- youtube_dl_scripts/lib/platform_config.sh
+++ youtube_dl_scripts/lib/platform_config.sh
@@ -5,7 +5,7 @@ function platform_config {
     then
         PLAYER_COMMAND='am start -n org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity -a android.intent.action.VIEW -d'
         HOME_DIR='/data/data/com.termux/files/home'
-        cd "$HOME_DIR/storage/shared/Videos"    
+        cd "$HOME_DIR/storage/shared/Download"    
     else
         PLAYER_COMMAND='vlc'
         HOME_DIR='/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI'
