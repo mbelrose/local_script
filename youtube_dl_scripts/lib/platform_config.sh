34b469b2fd6a32a06bf0aed4fc7bb58ecea3a187
mbelrose <mike@belrose.net>
2023-06-21 09:54:20 -0500

    switch to videos folder

--- youtube_dl_scripts/lib/platform_config.sh
+++ youtube_dl_scripts/lib/platform_config.sh
@@ -4,7 +4,7 @@ function platform_config {
     then
         PLAYER_COMMAND='am start -n org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity -a android.intent.action.VIEW -d'
         HOME_DIR='/data/data/com.termux/files/home'
-        cd "$HOME_DIR/storage/shared/Download"
+        cd "$HOME_DIR/storage/shared/Videos"    
     else
         PLAYER_COMMAND='vlc'
         HOME_DIR='/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI'
