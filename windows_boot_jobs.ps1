971832ef44413e33739869740bc8b6c9ae82d27f
mbelrose <mike@belrose.net>
2023-06-23 12:03:33 -0500

    add element for matrix

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -1,6 +1,9 @@
 # break reminder
 start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1'
 
+# Element for matrix
+& C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\element-desktop\Element.exe
+
 # Google Voice
 Start-Sleep -Seconds 20
 & 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://voice.google.com"
@@ -8,3 +11,5 @@ Start-Sleep -Seconds 20
 # Discord
 Start-Sleep -Seconds 20
 & 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://discord.com/channels/@me"
+
+
