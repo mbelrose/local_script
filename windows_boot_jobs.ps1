956e399b36164219dc0f42565e9df7793ee707ae
mbelrose <mike@belrose.net>
2023-07-05 17:27:22 -0500

    revert all this, requires obscure module

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -1,29 +1,16 @@
-# need to run:
-# Install-Module -Name VirtualDesktop
-
-# create a new desktop
-$targetDesktop = New-Desktop -PassThru
-
-# $desktopId = $desktop.DesktopId
-# $targetDesktop = Get-DesktopDesktopId -DesktopNumber 2
-# New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VirtualDesktops" -Name "Desktop 2" -ItemType "Directory"
-
 # break reminder
-$breakObj = Start-Process powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1' -PassThru
-Move-Window $breakObj.MainWindowHandle $targetDesktop
+start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1'
 
 # Element for matrix
-$elementObj = Start-Process -FilePath "C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\element-desktop\Element.exe" -PassThru
-Move-Window $elementObj.MainWindowHandle $targetDesktop
+& C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\element-desktop\Element.exe
 
 # Google Voice
-$voiceObj = Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "-new-window", "https://voice.google.com" -PassThru
-Move-Window $voiceObj.MainWindowHandle $targetDesktop
+Start-Sleep -Seconds 20
+& 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://voice.google.com"
 
 # Discord
-$discordObj = Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "-new-window", "https://discord.com/channels/@me" -PassThru
-Move-Window $discordObj.MainWindowHandle $targetDesktop
+Start-Sleep -Seconds 20
+& 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://discord.com/channels/@me"
 
 # Twitch Live Alert
-$alertObj = Start-Process powershell 'node \"C:\Program Files (x86)\twitch_live_alert_win\twitch_live_alert\server\TwitchLiveAlert.js\"' -PassThru
-Move-Window $alertObj.MainWindowHandle $targetDesktop
+start powershell 'node \"C:\Program Files (x86)\twitch_live_alert_win\twitch_live_alert\server\TwitchLiveAlert.js\"'
