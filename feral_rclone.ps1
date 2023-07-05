3b9efe33d4c6646b461cc12c552698cc66129906
mbelrose <mike@belrose.net>
2023-07-05 16:09:09 -0500

    changed host to chocolatey, no longer need paths

--- feral_rclone.ps1
+++ feral_rclone.ps1
@@ -1,3 +1,2 @@
-cd 'C:\Program Files (x86)\rclone\'
 $password = Get-Content 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\keys\feral_rclone_password.txt'
-.\rclone.exe rcd --rc-user mbelrose --rc-pass $password[0] --rc-web-gui
+& rclone.exe rcd --rc-user mbelrose --rc-pass $password[0] --rc-web-gui
