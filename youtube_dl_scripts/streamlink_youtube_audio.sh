2c9c0d5b0a82ec825d1b3cb36b7727d3fc87705a
mbelrose <mike@belrose.net>
2023-07-05 19:54:52 -0500

    wrong text handling

--- youtube_dl_scripts/streamlink_youtube_audio.sh
+++ youtube_dl_scripts/streamlink_youtube_audio.sh
@@ -1,5 +1,5 @@
 echo 'Enter streamer id:'
 read streamer_id
-streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
+streamer_id=$(echo $streamer_id | awk -F= '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
 streamlink https://www.youtube.com/watch?v=$streamer_id
