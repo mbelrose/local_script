3b764e22ba8289a5eed58ef2e8358d41068c8f5c
mbelrose <mike@belrose.net>
2023-08-07 08:54:15 -0500

    make google voice and twitch alerts optional

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -1,16 +1,26 @@
+# ask user which programs to run
+$wshell = New-Object -ComObject Wscript.Shell
+$google_voice = $wshell.Popup("Do you want to run Google Voice?",0,"Start Program",36)
+$twitch_alerts = $wshell.Popup("Do you want to run Twitch Alerts?",0,"Start Program",36)
+
 # break reminder
 start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1'
 
 # Element for matrix
 & C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\element-desktop\Element.exe
 
+
 # Google Voice
-Start-Sleep -Seconds 20
-& 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://voice.google.com"
+if ($google_voice -eq 6) {
+    Start-Sleep -Seconds 20
+    & 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://voice.google.com"
+}
 
 # Discord
 Start-Sleep -Seconds 20
 & 'C:\Program Files\Google\Chrome\Application\chrome.exe' -new-window "https://discord.com/channels/@me"
 
 # Twitch Live Alert
-start powershell 'node \"C:\Program Files (x86)\twitch_live_alert_win\twitch_live_alert\server\TwitchLiveAlert.js\"'
+if ($twitch_alerts -eq 6) {
+    start powershell 'node \"C:\Program Files (x86)\twitch_live_alert_win\twitch_live_alert\server\TwitchLiveAlert.js\"'
+}
