effc9147441210ba68c6d8dfb62aaa3d0701c597
mbelrose <mike@belrose.net>
2023-08-30 18:45:43 -0500

    add twitch mode

---
+++ youtube_dl_scripts/youtube-dl_video_stream_twitch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+# this script will not run from the "source" command on some platforms
+LIBRARY_DIR="$( dirname -- $0)/lib"
+source "$LIBRARY_DIR/platform_config.sh"
+
+platform_config
+
+echo 'Enter streamer id:'
+read streamer_id
+
+streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
+streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
+
+`$PLAYER_COMMAND $(yt-dlp -f "480p,480p30" -g https://twitch.tv/$streamer_id)`
