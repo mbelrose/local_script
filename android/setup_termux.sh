4a6ec91e81d27fd2f19a06094b8b800da360ab0a
mbelrose <mike@belrose.net>
2023-08-30 23:43:21 -0500

    add oh m zsh to termux

--- android/setup_termux.sh
+++ android/setup_termux.sh
@@ -1,3 +1,5 @@
+# put openai key in ~/.keys/openai_api_key
+
 pkg update
 pkg upgrade
 termux-setup-storage
@@ -15,3 +17,13 @@ pkg install libxslt
 pip install streamlink
 pkg install ffmpeg
 pip install yt-dlp
+
+cd ~/.local/opt
+git clone https://github.com/mbelrose/local_script.git
+
+#oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ~/.local/opt/local_script/oh_my_zsh_zshrc ~/.zshrc
+
+# restart shell then run this
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
