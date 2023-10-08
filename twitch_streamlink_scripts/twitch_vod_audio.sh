c94d666275a76ea956c41613ef1c786d5a886a37
mbelrose <mike@belrose.net>
2023-10-08 05:41:48 -0500

    move windows to second desktop

--- twitch_streamlink_scripts/twitch_vod_audio.sh
+++ twitch_streamlink_scripts/twitch_vod_audio.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/zsh
+
+WINDOWS_HOME='/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI'
+LIB_DIRECTORY=$WINDOWS_HOME/Documents/local_script/lib
+WORKING_DIRECTORY=$HOME/webdev_repositories_personal/twitch_live_alert
+
+# import move window function
+source $LIB_DIRECTORY/move_window.sh
+
 echo 'Enter vod id:'
 read streamer_id
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
-streamlink https://twitch.tv/videos/$streamer_id audio_only,audio
-
+gnome-terminal --title "Streamlink" \
+    --working-directory $WORKING_DIRECTORY \
+    -- streamlink https://twitch.tv/videos/$streamer_id audio_only,audio
+move_window "Streamlink" 1
