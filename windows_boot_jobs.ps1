d4ce069e489765795faa97617ff1e5dd34d80082
mbelrose <mike@belrose.net>
2023-10-09 05:22:25 -0500

    these delay loops were probably not needed

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -9,30 +9,21 @@ start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_rem
 # Element for matrix
 & C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\element-desktop\Element.exe
 
-
 # Google Voice
 if ($google_voice -eq 6) {
-    Start-Sleep -Seconds 20
     & 'C:\Program Files\Mozilla Firefox\firefox.exe' -new-window "https://voice.google.com/u/0/messages"
 }
 
 # Discord
-Start-Sleep -Seconds 20
 & 'C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\Discord\Update.exe' --processStart Discord.exe
-
 
 # Twitch Live Alert
 if ($twitch_alerts -eq 6) {
     start powershell 'node \"C:\Program Files (x86)\twitch_live_alert_win\twitch_live_alert\server\TwitchLiveAlert.js\"'
 }
 
-
 # Firefox
-Start-Sleep -Seconds 20
 & 'C:\Program Files\Mozilla Firefox\firefox.exe' -private-window 
 
-
-
 # background wallpaper switcher
-Start-Sleep -Seconds 30
 & "C:\Program Files (x86)\johnsadventures.com\John's Background Switcher\BackgroundSwitcher.exe"
