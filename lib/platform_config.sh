3bae9096abd3fc1630ca79ac156cba12cea7f3f0
mbelrose <mike@belrose.net>
2023-10-08 19:01:34 -0500

    use the platform_config for android support

---
+++ lib/platform_config.sh
@@ -0,0 +1,14 @@
+# set platform specific variables
+
+function platform_config {
+    if [[ "$(uname -o)" == 'Android' ]]
+    then
+        PLAYER_COMMAND='am start -n org.videolan.vlc/org.videolan.vlc.gui.video.VideoPlayerActivity -a android.intent.action.VIEW -d'
+        HOME_DIR='/data/data/com.termux/files/home'
+        cd "$HOME_DIR/storage/shared/Download"    
+    else
+        PLAYER_COMMAND='vlc'
+        HOME_DIR='/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI'
+        cd "$HOME_DIR/Videos/videos_to_move"
+    fi
+}
--- twitch_streamlink_scripts/twitch_audio.sh
+++ twitch_streamlink_scripts/twitch_audio.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/zsh
 
-WINDOWS_HOME='/media/mint/Local Disk/Users/user.DESKTOP-6UBKKRI'
-LIB_DIRECTORY=$WINDOWS_HOME/Documents/local_script/lib
-WORKING_DIRECTORY=$HOME/webdev_repositories_personal/twitch_live_alert
+# this script will not run from the "source" command on some platforms
+LIBRARY_DIR="$( dirname -- $0)/lib"
+source "$LIBRARY_DIR/platform_config.sh"
+
+platform_config
 
 # import move window function
-source $LIB_DIRECTORY/move_window.sh
+source $LIBRARY_DIR/move_window.sh
 
 echo 'Enter streamer id:'
 read streamer_id
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
 gnome-terminal --title "Streamlink" \
-    --working-directory $WORKING_DIRECTORY \
+    --working-directory $HOME_DIR \
     -- streamlink https://twitch.tv/$streamer_id audio_only,audio
 move_window "Streamlink" 1
