53e39c2a490b6d625444742e78e5d0a3e13b21ed
mbelrose <mike@belrose.net>
2023-10-10 01:59:44 -0500

    store results in clipboard

--- qr_clipboard.sh
+++ qr_clipboard.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/zsh
 
-# print clipboard to a qr code
-CLIPBOARD=$(xclip -selection c -o)
-echo $CLIPBOARD | qrencode -t utf8
-echo $CLIPBOARD
-echo 'Press Enter to continue.'
-read
+#convert clipboard to a qr code
+xclip -selection c -o | qrencode -t utf8 | xclip -selection c
