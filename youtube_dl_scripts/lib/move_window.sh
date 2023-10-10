f9cd8703ae8b51ffe35fa3b266bad44fc49df702
mbelrose <mike@belrose.net>
2023-10-09 20:03:58 -0500

    add move window function to some yt-dlp scripts

---
+++ youtube_dl_scripts/lib/move_window.sh
@@ -0,0 +1,46 @@
+# move a window to a different desktop
+function move_window() {
+
+    local window_title=$1
+    local desktop_number=$2
+    SEARCH_LOOP_LIMIT=120
+    MOVE_LOOP_LIMIT=120
+
+    # match the window title, partial match, caseless
+    search_loop_counter=0
+    window_list=""
+    while [ ${#window_list} -eq 0 ] \
+        && [ "$search_loop_counter" -le "$SEARCH_LOOP_LIMIT" ]
+    do
+        sleep 1
+        window_list=$(wmctrl -l)
+        window_list=$(
+            awk -v title="$window_title" \
+            'BEGIN {IGNORECASE=1} $0 ~ title { print $1 }' \
+            <<< "$window_list")
+        window_list=$( \
+            grep '^[[:alnum:]]\+$' <<< "$window_list" \
+        )
+        ((search_loop_counter++))
+    done
+
+    # move the window
+    while IFS= read -r window_id; do
+        if [ ${#window_id} -gt 0 ] # this probably contradicts the while loop
+        then
+            current_desktop=-1
+            loop_counter=0
+            while [ "$desktop_number" -ne "$current_desktop" ] \
+                && [ "$loop_counter" -le "$MOVE_LOOP_LIMIT" ]
+            do
+                sleep 1
+                wmctrl -ir "$window_id" -t "$desktop_number"
+                current_desktop=$(wmctrl -lG \
+                    | awk -v id="$window_id" \
+                    '$1 == id { print $2 }')
+                ((loop_counter++))
+            done
+        fi
+    done <<< "$window_list"
+
+}
--- youtube_dl_scripts/youtube-dl_video_stream_twitch_vod.sh
+++ youtube_dl_scripts/youtube-dl_video_stream_twitch_vod.sh
@@ -6,10 +6,21 @@ source "$LIBRARY_DIR/platform_config.sh"
 
 platform_config
 
+# import move window function
+source $LIBRARY_DIR/move_window.sh
+
 echo 'Enter vod id:'
 read streamer_id
 
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
 
-`$PLAYER_COMMAND $(yt-dlp -f "480p,480p30" -g https://twitch.tv/videos/$streamer_id)`
+if [[ "$(uname -o)" == 'Android' ]]
+then
+    `$PLAYER_COMMAND $(yt-dlp -f "480p,480p30" -g https://twitch.tv/videos/$streamer_id)`
+else
+    gnome-terminal --title "Streamlink" \
+        --working-directory $HOME_DIR \
+        -- $PLAYER_COMMAND $(yt-dlp -f "480p,480p30" -g https://twitch.tv/videos/$streamer_id)
+    move_window "Streamlink" 1
+fi
