ec25cac8ffa26ec3acbd88a9466b2a2c66b9ebdd
mbelrose <mike@belrose.net>
2023-10-10 21:13:27 -0500

    fix command line ref

--- qr_code_png.sh
+++ qr_code_png.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
 # take a filename from command line and convert it to a qr code
-qrencode -r qr_filename -o /tmp/qr_code.png 
+qrencode -r $1 -o /tmp/qr_code.png 
 xviewer /tmp/qr_code.png 
