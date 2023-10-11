0c92b169fbe6f6f8b0d84b3c069bb419e44d10d6
mbelrose <mike@belrose.net>
2023-10-10 21:16:49 -0500

    cleanup temp file

--- qr_code_png.sh
+++ qr_code_png.sh
@@ -3,3 +3,6 @@
 # take a filename from command line and convert it to a qr code
 qrencode -r $1 -o /tmp/qr_code.png 
 xviewer /tmp/qr_code.png 
+echo 'Press enter key to continue.'
+read
+rm /tmp/qr_code.png 
