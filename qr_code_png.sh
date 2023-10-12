3507b50334050259d9c30d1e38ffd8798e72cb35
mbelrose <mike@belrose.net>
2023-10-12 17:37:33 -0500

    use random filename to avoid name collisions

--- qr_code_png.sh
+++ qr_code_png.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/zsh
 
 # take a filename from command line and convert it to a qr code
-qrencode -r $1 -o /tmp/qr_code.png 
-xviewer /tmp/qr_code.png 
+
+qr_filename=$(uuidgen)
+qr_filename="/tmp/qr_code_${qr_filename}.png"
+
+qrencode -r $1 -o $qr_filename
+xviewer $qr_filename
 echo 'Press enter key to continue.'
 read
-rm /tmp/qr_code.png 
+rm $qr_filename
