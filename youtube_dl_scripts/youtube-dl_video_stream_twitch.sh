d592666968f991ba49d4c57c241166935ee62418
mbelrose <mike@belrose.net>
2023-10-26 10:45:44 -0500

    accept streamer_id from command line

--- youtube_dl_scripts/youtube-dl_video_stream_twitch.sh
+++ youtube_dl_scripts/youtube-dl_video_stream_twitch.sh
@@ -9,8 +9,13 @@ platform_config
 # import move window function
 source $LIBRARY_DIR/move_window.sh
 
-echo 'Enter streamer id:'
-read streamer_id
+if [[ $# -eq 0 ]]
+then
+    echo 'Enter streamer id:'
+    read streamer_id
+else
+    streamer_id=$1
+fi
 
 streamer_id=$(echo $streamer_id | awk -F/ '{print $NF}')
 streamer_id=$(echo $streamer_id | sed 's/[^[:alnum:]_\?=]//g')
