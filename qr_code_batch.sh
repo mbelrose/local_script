9d5eccfde6369feb220474441efbc513ee90f25b
mbelrose <mike@belrose.net>
2023-12-12 01:30:50 -0600

    adding in base url

--- qr_code_batch.sh
+++ qr_code_batch.sh
@@ -2,10 +2,10 @@
 # take a list of parital urls as first parameter
 # and transform into qr codes
 # partial urls should be alphanumeric lowercase and dashes
+base_url="https://www.xenofera.com/xenopedia/"
 
 while read partial_url; do
-    url="$partial_url"
+    url="$base_url$partial_url"
     qr_filename="${partial_url}_qrcode.png"
-    $url | qrencode -o $qr_filename
-
+    echo $url | qrencode -o $qr_filename
 done < $1
