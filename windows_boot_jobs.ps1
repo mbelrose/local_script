9a2056aee346a232fbaf330a0b79049459083483
mbelrose <mike@belrose.net>
2024-03-20 15:26:53 -0500

    remove elemet, google voice, and background switcher

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -1,13 +1,9 @@
 # ask user which programs to run
 $wshell = New-Object -ComObject Wscript.Shell
-$google_voice = $wshell.Popup("Do you want to run Google Voice?",0,"Start Program",36)
 $twitch_alerts = $wshell.Popup("Do you want to run Twitch Alerts?",0,"Start Program",36)
 
 # break reminder
 start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1'
-
-# Element for matrix
-& C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\element-desktop\Element.exe
 
 # initialize firefox
 & 'C:\Program Files\Mozilla Firefox\firefox.exe' -private-window
@@ -23,11 +19,3 @@ if ($twitch_alerts -eq 6) {
 # Freetube Youtube
 & 'C:\Program Files\FreeTube\FreeTube.exe'
 
-# background wallpaper switcher
-& "C:\Program Files (x86)\johnsadventures.com\John's Background Switcher\BackgroundSwitcher.exe"
-
-# Google Voice
-if ($google_voice -eq 6) {
-    Start-Sleep -Seconds 120
-    & 'C:\Program Files\Mozilla Firefox\firefox.exe' -private-window -new-window "https://voice.google.com/u/0/messages"
-}
