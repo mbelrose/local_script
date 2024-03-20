4e131604a392abcfd804823263a7eb55ea1169f3
mbelrose <mike@belrose.net>
2024-03-20 15:28:38 -0500

    no need to autorun firefox, freetube

--- windows_boot_jobs.ps1
+++ windows_boot_jobs.ps1
@@ -5,9 +5,6 @@ $twitch_alerts = $wshell.Popup("Do you want to run Twitch Alerts?",0,"Start Prog
 # break reminder
 start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_reminder.ps1'
 
-# initialize firefox
-& 'C:\Program Files\Mozilla Firefox\firefox.exe' -private-window
-
 # Discord
 & 'C:\Users\user.DESKTOP-6UBKKRI\AppData\Local\Discord\Update.exe' --processStart Discord.exe
 
@@ -15,7 +12,3 @@ start powershell 'C:\Users\user.DESKTOP-6UBKKRI\Documents\local_script\break_rem
 if ($twitch_alerts -eq 6) {
     start powershell 'node \"C:\Program Files (x86)\twitch_live_alert_win\twitch_live_alert\server\TwitchLiveAlert.js\"'
 }
-
-# Freetube Youtube
-& 'C:\Program Files\FreeTube\FreeTube.exe'
-
