6136008449e4050941b50f48e934463d0eb0a537
Michael T. Belrose <mike@belrose.net>
2025-02-26 16:48:51 -0600

    custom docker update script

---
+++ docker_pull_all.sh
@@ -0,0 +1,28 @@
+#!
+cd ~/.local/opt/redlib
+docker compose down
+git pull
+docker pull quay.io/redlib/redlib
+docker compose up -d
+
+cd ~/.local/opt/invidious
+docker compose down
+git pull
+docker pull quay.io/invidious/invidious
+docker pull quay.io/invidious/inv-sig-helper
+docker pull quay.io/invidious/youtube-trusted-session-generator
+docker pull postgres
+docker compose up -d
+
+cd ~/.local/opt/cobalt
+docker compose down
+git pull
+docker pull ghcr.io/imputnet/cobalt
+docker pull ghcr.io/containrrr/watchtower
+docker compose up -d
+
+cd ~/.local/opt/cobalt_web
+docker compose down
+#no git repo to pull here
+docker pull lipanski/docker-static-website
+docker compose up -d
