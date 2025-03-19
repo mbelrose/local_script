4e8c5a2a1a2c4086980d05e32bbd917807db593e
Michael T. Belrose <mike@belrose.net>
2025-03-18 22:03:13 -0500

    add script for Glance aggregator

---
+++ glance.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/zsh
+
+#the script must run as a user with permission to run systemctl as root
+
+timeout=60
+working_directory=$HOME/.local/opt/glance
+glance_server_url="http://127.0.0.1:9700/"
+
+# initialize firefox so further calls don't fork processes
+nohup firefox -private-window >/dev/null 2>/dev/null &
+
+cd $working_directory
+
+# start docker
+if ! systemctl is-active --quiet docker;
+then 
+        systemctl start docker
+fi
+
+# wait for docker to start
+docker_loop_counter=0
+while [[ $docker_loop_counter -le $timeout ]]
+do
+        sleep 1
+        if systemctl is-active --quiet docker;
+        then
+
+# --------
+# --------
+# --------
+
+# start invidious server
+if !  curl -s $glance_server_url --output /dev/null;
+then
+        docker compose up -d
+fi
+
+# wait for invidious server to start
+invidious_container_loop_counter=0
+while [[ $invidious_container_loop_counter -le $timeout ]]
+do
+        sleep 1
+        if  curl -s $glance_server_url --output /dev/null;
+        then
+                # open invidious server in firefox
+                firefox -private-window $glance_server_url >/dev/null 2>/dev/null
+                
+                break
+        fi
+        ((invidious_container_loop_counter++))
+done
+
+# -------
+# -------
+# -------
+                break
+        fi
+        ((docker_loop_counter++))
+done
