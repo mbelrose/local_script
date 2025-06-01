e8de70d079d0887ed754d5c6e8309b85e5125aef
Michael T. Belrose <mike@belrose.net>
2025-06-01 00:25:42 -0500

    disable cobalt

--- docker_pull_all.sh
+++ docker_pull_all.sh
@@ -22,11 +22,11 @@ docker pull ghcr.io/imputnet/cobalt
 docker pull ghcr.io/containrrr/watchtower
 docker compose up -d
 
-cd ~/.local/opt/cobalt_web
-docker compose down
-#no git repo to pull here
-docker pull lipanski/docker-static-website
-docker compose up -d
+# cd ~/.local/opt/cobalt_web
+# docker compose down
+# #no git repo to pull here
+# docker pull lipanski/docker-static-website
+# docker compose up -d
 
 cd ~/.local/opt/glance
 docker compose down
