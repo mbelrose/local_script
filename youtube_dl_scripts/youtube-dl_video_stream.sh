0a6d506a8173d6e36575f43f7f936049bdb7f8ce
Michael T. Belrose <mike@belrose.net>
2025-10-11 21:43:10 -0500

    merge youtube stream clipboard feature  into main

--- youtube_dl_scripts/youtube-dl_video_stream.sh
+++ youtube_dl_scripts/youtube-dl_video_stream.sh
@@ -9,4 +9,13 @@ platform_config
 echo 'Enter video id:'
 read stream_url
 
-`$PLAYER_COMMAND $(yt-dlp -f "best[height<=1080]" -g $stream_url)`
+if [[ -z "$stream_url" ]]; then
+    echo "No video id provided."
+    echo "Press Enter to get the video id from the clipboard."
+    read
+    stream_url=$(xclip -o -selection c )
+fi
+
+if [[ -z "$stream_url" ]]; then
+    `$PLAYER_COMMAND $(yt-dlp -f "best[height<=1080]" -g $stream_url)`
+fi
--- youtube_dl_scripts/youtube-dl_video_stream_clipboard.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-# this script will not run from the "source" command on some platforms
-LIBRARY_DIR="$( dirname -- $0)/lib"
-source "$LIBRARY_DIR/platform_config.sh"
-
-platform_config
-
-`$PLAYER_COMMAND $(yt-dlp -f "best[height<=1080]" -g $(xclip -o -selection c ))`
